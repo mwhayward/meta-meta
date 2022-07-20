175e505e4d7ae7608bcaa84647c8d7b94762944b
Morgan <mh653@leicester.ac.uk>
2022-07-20 10:07:58 +0100

    metabolites table complete

--- src/bmrb_reader.py
+++ src/bmrb_reader.py
@@ -6,6 +6,8 @@ import os
 import sqlite3
 from sqlite3 import Error
 
+import pandas as pd
+
 
 class BMRB_Reader:
     def __init__(self, directory):
@@ -23,9 +25,45 @@ class BMRB_Reader:
 
     def run(self):
         count = 0
-        metabolites = {}
+        # define the dictionaries for populating
+        metabolites = {'metabolite_id': [],
+                       'name': [],
+                       'description': [],
+                       'chemical_formula': [],
+                       'molecular_weight': [],
+                       'smiles': [],
+                       'inChi': []}
+        samples = {'sample_id': [],
+                   'metabolite_id': [],
+                   'pH': [],
+                   'amount': [],
+                   'reference': []}
+        spectra = {'spectrum_id': [],
+                   'sample_id': [],
+                   'temperature': [],
+                   'frequency': []}
+        multiplets = {'multiplet_id': [],
+                      'spectrum_id': [],
+                      'center': [],
+                      'atom_ref': [],
+                      'multiplicity': []}
+        peaks = {'peak_id': [],
+                 'spectrum_id': [],
+                 'multiplet_id': [],
+                 'shift': [],
+                 'intensity': [],
+                 'width': []}
+        synonyms = {'metabolite_id': [],
+                    'synonym': []}
+        isin = {'metabolite_id': [],
+                'group': []}
+        ontology = {'group': [],
+                    'definition': []}
+
+        # dictionaries for search terms in BMSE files
         targets = {'name': ['save_assembly_1'],
                    'chemical_formula': ['Chem_comp.Formula'],
+                   'smiles': ['Chem_comp_SMILES'],
                    'inChi': ['Chem_comp.InChI_code'],
                    'pH': ['save_sample_conditions_1', 'save_conditions_1', 'save_conditions_1H_DW'],
                    'amount': ['save_sample_1', 'save_sample_1H_050115_P00_02_IS_DW'],
@@ -42,27 +80,33 @@ class BMRB_Reader:
         for num, file in enumerate(files):
             if file.endswith('.str'):
                 print(f'{num + 1} out of {len(files)}:    {file}')
-                for readfile in nmrstarlib.read_files(str(target_dir.joinpath(file))):
-                    if 'NMR quality control of fragment libraries for screening' in readfile['save_entry_information'][
-                       'Entry.Title']:
-                        name = readfile['save_assembly_1']['Assembly.Name'].strip('\n')
+                for tree in nmrstarlib.read_files(str(target_dir.joinpath(file))):
+                    if 'NMR quality control of fragment libraries for screening' in tree['save_entry_information']['Entry.Title']:
+                        name = tree['save_assembly_1']['Assembly.Name'].strip('\n')
                     else:
-                        name = readfile['save_entry_information']['Entry.Title'].strip('\n')
-                    if name not in metabolites.keys():
-                        metabolites[name] = [readfile['data']]
-                    else:
-                        metabolites[name].append(readfile['data'])
-        print(metabolites)
-        print(count)
+                        name = tree['save_entry_information']['Entry.Title'].strip('\n')
+                    if name not in metabolites['name']:
+                        metabolites['metabolite_id'].append(count)
+                        count += 1
+                        metabolites['name'].append(name)
+                        metabolites['description'].append(None)
+                        metabolites['chemical_formula'].append(self.find_save_data(tree, 'Chem_comp.Formula'))
+                        metabolites['molecular_weight'].append(self.find_save_data(tree, 'Chem_comp.Formula_weight'))
+                        metabolites['smiles'].append(self.find_save_data(tree, 'Chem_comp_SMILES'))
+                        metabolites['inChi'].append(self.find_save_data(tree, 'Chem_comp.InChI_code'))
+        metabolites_df = pd.DataFrame(metabolites)
+        print(metabolites_df)
 
     def find_save_data(self, tree, target):
         # returns the first instance of save data that matches the given target
+        # doesn't delve into loops
         for save in tree.keys():
             if not isinstance(tree[save], str):
                 for entity in tree[save].keys():
                     if target == entity:
                         return tree[save][entity]
         print(f'no {target} in {tree["data"]}')
+        return None
 
     def check_exists(self):
         pass
